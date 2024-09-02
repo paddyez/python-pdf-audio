ed26277ec8babddcc44a521503ff4e50e5cdc171
Patrick-Emil Zörner <paddyez@gmail.com>
2024-09-02 20:25:10 +0200

    indent; globals for now

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
+"""Module providing RegEx"""
 import re
 import pyttsx3
 from PyPDF2 import PdfReader
 
 def read_pdf():
-  pdf = PdfReader('book.pdf')
-  return ' '.join([re.sub(r'\s+', ' ', page.extract_text().strip()) for page in pdf.pages])
+    """Function parse file"""
+    pdf = PdfReader('book.pdf')
+    return ' '.join([re.sub(r'\s+', ' ', page.extract_text().strip()) for page in pdf.pages])
 
-def save_audio(read_pdf):
-  speaker.save_to_file(read_pdf, 'book.ogg')
-  speaker.runAndWait()
-  speaker.stop()
+def save_audio():
+    """Function save file to ogg"""
+    speaker.save_to_file(STRIPPED_TEXT, 'book.ogg')
+    speaker.runAndWait()
+    speaker.stop()
 
-def speak(read_pdf):
-  speaker.say(read_pdf)
-  speaker.runAndWait()
-  speaker.stop()
+def speak():
+    """Function read out"""
+    speaker.say(STRIPPED_TEXT)
+    speaker.runAndWait()
+    speaker.stop()
 
-def change_voice(speaker, language, gender='VoiceGenderFemale'):
-  for voice in speaker.getProperty('voices'):
-    #print(voice.languages)
-    #print(voice.gender)
-    if language in voice.languages and gender == voice.gender:
-      speaker.setProperty('voice', voice.id)
-      return True
-  raise RuntimeError("Language '{}' for gender '{}' not found".format(language, gender))
+def change_voice(gender='VoiceGenderFemale'):
+    """Function change voice"""
+    for voice in speaker.getProperty('voices'):
+        #print(voice.languages)
+        #print(voice.gender)
+        if ENGLISH in voice.languages and gender == voice.gender:
+            speaker.setProperty('voice', voice.id)
+            return True
+    raise RuntimeError(f"Language '{ENGLISH}' for gender '{gender}' not found")
+
+speaker = pyttsx3.init()
+STRIPPED_TEXT = read_pdf()
+ENGLISH = b'\x02en-gb'
+DEUTSCH = b'\x05de'
 
 if __name__ == '__main__':
-  stripped_text = read_pdf()
-  #print(stripped_text)
-  speaker = pyttsx3.init()
-  english = b'\x02en-gb'
-  deutsch = b'\x05de'
-  change_voice(speaker, english, 'male')
-  speaker.setProperty('rate', 160)
-  speak(stripped_text)
-  save_audio(stripped_text)
+    #print(stripped_text)
+    change_voice('male')
+    speaker.setProperty('rate', 160)
+    speak()
+    save_audio()
